1847f6144849b38ca85ac3a915ec14399e8039ea
azizaldhubri <azizaldhubri@gmail.com>
2025-05-02 04:17:15 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Category;
-use App\Models\Product;
-use App\Models\ProductImage;
+ 
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -17,9 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(1)->withToken()->create();
-        Category::factory(50)->create();
-        Product::factory(100)->create();
-        ProductImage::factory(400)->create();
+        User::factory(1)->withToken()->create();
+ 
     }
 }
